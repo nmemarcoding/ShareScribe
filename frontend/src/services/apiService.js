6d0a3eda3b9dd2403819d88200f0aa284b14cd34
nima memarzadeh <nmemarcoding@gmail.com>
2025-08-05 10:37:00 -0700

    feat: Update API base URL to production endpoint

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.16:8080/api',
+//   baseURL: 'http://192.168.1.16:8080/api',
+     baseURL: 'https://springboot-service-55670318776.us-central1.run.app/api',
 
 });
 
