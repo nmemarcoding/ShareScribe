f4c788562780846ab609e8bc811a4bd9f7e4cd24
nima memarzadeh <nmemarcoding@gmail.com>
2025-09-29 11:14:51 -0700

    feat: Update file permissions for multiple project files and adjust apiService.js baseURL for local development

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-//   baseURL: 'http://192.168.1.16:8080/api',
-     baseURL: 'https://sharescribe-service-55670318776.us-central1.run.app/api',
+  baseURL: 'http://localhost:8080/api',
+    
 
 });
 
